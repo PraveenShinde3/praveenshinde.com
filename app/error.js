73f72903c87f77b0e533668d972ad0e8fbee85bd
Praveen Shinde <shindepraveen002@gmail.com>
2023-12-03 12:19:53 +0530

    Fix : Quote ESLINT error in error.js page

--- app/error.js
+++ app/error.js
@@ -5,8 +5,8 @@ const error = () => {
   return (
     <div className=" h-screen w-screen flex justify-center items-center gap-2">
       <p className=" text-center">
-        "<b>Oops! Something went wrong.</b> Please try again later. For
-        assistance, contact shinde.praveen.dev@gmail.com"{" "}
+        &quot;<b>Oops! Something went wrong.</b> Please try again later. For
+        assistance, contact shinde.praveen.dev@gmail.com&quot;
       </p>
     </div>
   );
