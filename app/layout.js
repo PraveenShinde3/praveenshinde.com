f5711e135b0c06e499c1d5fbf323816af0b44cbe
Praveen Shinde <shindepraveen002@gmail.com>
2024-12-16 10:59:02 +0530

    Added meta twitter image

--- app/layout.js
+++ app/layout.js
@@ -41,8 +41,14 @@ export const metadata = {
     title: "Praveen Shinde - Portfolio",
     description:
       "Explore the personal projects, achievements, and expertise of Praveen Shinde in web development and UI design.",
-    image:
-      "https://res.cloudinary.com/dckc5lez0/image/upload/v1734101206/Portfolio_wckv4i.png",
+    image: {
+      url: "https://res.cloudinary.com/dckc5lez0/image/upload/v1734101206/Portfolio_wckv4i.png",
+      blurDataURL:
+        "https://res.cloudinary.com/dckc5lez0/image/upload/v1734101206/Portfolio_wckv4i.png",
+      width: 1200,
+      height: 630,
+      alt: "Protfolio",
+    },
   },
   themeColor: "#3178c6", // Adjust based on the site's primary color
   favicon: "/favicon.ico",
@@ -52,6 +58,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/dckc5lez0/image/upload/v1734101206/Portfolio_wckv4i.png"
+        />
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <main className="text-foreground flex justify-center">
