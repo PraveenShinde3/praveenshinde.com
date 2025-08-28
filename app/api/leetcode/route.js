8aaa9ce19189e36196a2d50f1e2c2b12d0bf3bac
Praveen Shinde <shindepraveen002@gmail.com>
2025-08-28 22:07:18 +0530

    fix: added no cache control for leetcode api

--- app/api/leetcode/route.js
+++ app/api/leetcode/route.js
@@ -78,6 +78,8 @@ export async function GET(request) {
               "User-Agent":
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
               Referer: "https://leetcode.com",
+              "Cache-Control": "no-cache",
+              Pragma: "no-cache",
             },
             body: JSON.stringify({
               query: problemQuery,
