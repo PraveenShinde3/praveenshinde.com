8c9d30209720c98ffca647bcbad4101a9a124d47
Praveen Shinde <shindepraveen002@gmail.com>
2025-12-31 11:13:48 +0530

    feat: Added DSA-Doc project and kept only active project

--- app/components/ProjectSection.js
+++ app/components/ProjectSection.js
@@ -6,11 +6,23 @@ import CertificateHubThumbnail from "../../public/CertificateHubthum.jpg";
 import SolveAlgoThumbnail from "../../public/solveAlgoThumb.png";
 import TailwindAnimationThumbnail from "../../public/tailwind-animations.png";
 import DesignDexThumbnail from "../../public/DesignDexThumbnail.png";
+import DsaDocThumbnail from "../../public/Dsa-doc-thumbnail.png";
 
 const ProjectSection = () => {
   const projectData = [
     {
       id: "01",
+      imageUrl: DsaDocThumbnail,
+      projectData: {
+        title: "DSA Documentation",
+        desc: "A comprehensive resource for learning and mastering Data Structures and Algorithms (DSA), designed to help programmers of all levels enhance their coding skills and problem-solving abilities.",
+        tag: "Web Application",
+        tech: "Next Js",
+        link: "https://dsa-doc.vercel.app/",
+      },
+    },
+    {
+      id: "02",
       imageUrl: TailwindAnimationThumbnail,
       projectData: {
         title: "Tailwind CSS Animations",
@@ -20,39 +32,39 @@ const ProjectSection = () => {
         link: "https://tailwind-css-animations.vercel.app/",
       },
     },
-    {
-      id: "02",
-      imageUrl: DesignDexThumbnail,
-      projectData: {
-        title: "DesignDex",
-        desc: "Unlock a world of design possibilities with our curated collection of resources, tools, and inspiration.",
-        tag: "Web Application",
-        tech: "Next Js, Supabase",
-        link: "https://designdex.vercel.app/",
-      },
-    },
-    {
-      id: "03",
-      imageUrl: SolveAlgoThumbnail,
-      projectData: {
-        title: "SolveAlgo",
-        desc: "Algorithms are the backbone of coding, helping you approach complex challenges with clarity and creativity.",
-        tag: "Web Application",
-        tech: "Next Js",
-        link: "https://solve-algo.vercel.app/",
-      },
-    },
-    {
-      id: "04",
-      imageUrl: CertificateHubThumbnail,
-      projectData: {
-        title: "Certificate Hub",
-        desc: "Creates secure, random passwords for users to use for their various online accounts and information.Creates secure, random passwords for users to use for their various online accounts and information.",
-        tag: "Web Application",
-        tech: "Next Js & Supabase",
-        link: "https://certificatehub.vercel.app/",
-      },
-    },
+    // {
+    //   id: "03",
+    //   imageUrl: DesignDexThumbnail,
+    //   projectData: {
+    //     title: "DesignDex",
+    //     desc: "Unlock a world of design possibilities with our curated collection of resources, tools, and inspiration.",
+    //     tag: "Web Application",
+    //     tech: "Next Js, Supabase",
+    //     link: "https://designdex.vercel.app/",
+    //   },
+    // },
+    // {
+    //   id: "04",
+    //   imageUrl: SolveAlgoThumbnail,
+    //   projectData: {
+    //     title: "SolveAlgo",
+    //     desc: "Algorithms are the backbone of coding, helping you approach complex challenges with clarity and creativity.",
+    //     tag: "Web Application",
+    //     tech: "Next Js",
+    //     link: "https://solve-algo.vercel.app/",
+    //   },
+    // },
+    // {
+    //   id: "05",
+    //   imageUrl: CertificateHubThumbnail,
+    //   projectData: {
+    //     title: "Certificate Hub",
+    //     desc: "Creates secure, random passwords for users to use for their various online accounts and information.Creates secure, random passwords for users to use for their various online accounts and information.",
+    //     tag: "Web Application",
+    //     tech: "Next Js & Supabase",
+    //     link: "https://certificatehub.vercel.app/",
+    //   },
+    // },
     // {
     //   id: "05",
     //   imageUrl: GeneratePassImg,
